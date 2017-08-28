947bda5a1c1a40ff2c61d799a1918d7c0f61adf2
ondreian <benjamin.clos+github@gmail.com>
2017-08-28 16:41:48 -0400

    remove dryrun from uploads

--- scripts/util/aws.rb
+++ scripts/util/aws.rb
@@ -1,6 +1,6 @@
 module AWS
   def self.cp(bucket, remote_file, local_file)
-    `aws s3 cp #{local_file} s3://#{bucket}/#{remote_file} --dryrun`
+    `aws s3 cp #{local_file} s3://#{bucket}/#{remote_file}`
   end
 
   def self.delete(bucket, prefix)
